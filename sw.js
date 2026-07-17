ff6545e8f0f1ecad9664a56a71649ab4f60d1365
MeisamMahmoodi <160858096+MeisamMahmoodi@users.noreply.github.com>
2026-07-18 00:32:35 +0200

    Neues MOL-Logo als App-Icon

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@
    Update-Hinweis in der App an.
    ========================================================================= */
 
-const CACHE_VERSION = "v3";
+const CACHE_VERSION = "v4";
 const CACHE_NAME = `musik-app-${CACHE_VERSION}`;
 
 const APP_SHELL = [
